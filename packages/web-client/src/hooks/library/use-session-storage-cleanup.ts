1a9e8342c401f97bd8ed8083fb497c73ace55190
Robert Piosi <robertpiosik@gmail.com>
2023-12-20 17:10:15 +0100

    Store uuids as strings (#158)
    Closes #157

--- packages/web-client/src/hooks/library/use-session-storage-cleanup.ts
+++ packages/web-client/src/hooks/library/use-session-storage-cleanup.ts
@@ -2,18 +2,16 @@ import { useEffect } from 'react'
 import useUpdateEffect from 'beautiful-react-hooks/useUpdateEffect'
 import { useSearchParams } from 'next/navigation'
 
-const HISTORY_LIMIT = 5
+const HISTORY_LIMIT = 10
 
 // Purpose of this hook is to avoid hitting storage limits.
 export const use_session_storage_cleanup = () => {
   const query_params = useSearchParams()
 
   useUpdateEffect(() => {
-    const query_params_history: string[] = [
-      ...new Set<string>(
-        JSON.parse(sessionStorage.getItem('query_params_history') || '[""]'),
-      ),
-    ]
+    const query_params_history: string[] = JSON.parse(
+      sessionStorage.getItem('query_params_history') || '[""]',
+    )
 
     if (query_params_history.length == HISTORY_LIMIT) {
       const oldest_query_params = query_params_history[0]
@@ -27,7 +25,12 @@ export const use_session_storage_cleanup = () => {
 
     sessionStorage.setItem(
       'query_params_history',
-      JSON.stringify([...query_params_history, query_params.toString()]),
+      JSON.stringify([
+        ...query_params_history.filter(
+          (entry) => entry != query_params.toString(),
+        ),
+        query_params.toString(),
+      ]),
     )
   }, [query_params])
 
