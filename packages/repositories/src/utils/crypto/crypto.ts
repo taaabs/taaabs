710ce8bd61c7fde8134411f41ac1b569274a43f9
Robert Piosik <robertpiosik@gmail.com>
2024-06-25 13:47:46 +0200

    Stored encryption key format (#55)

--- packages/repositories/src/utils/crypto/crypto.ts
+++ packages/repositories/src/utils/crypto/crypto.ts
@@ -19,7 +19,6 @@ export namespace Crypto {
     return hash
   }
 
-  // Hash of concatenated data and key.
   export const SHA256 = async (
     data: string,
     encryption_key: Uint8Array,
@@ -43,64 +42,73 @@ export namespace Crypto {
       data: string,
       encryption_key: Uint8Array,
     ): Promise<string> => {
-      const encoded_data = new TextEncoder().encode(data)
-      const iv = window.crypto.getRandomValues(new Uint8Array(16)) // Generate a random IV
+      try {
+        const encoded_data = new TextEncoder().encode(data)
+        const iv = window.crypto.getRandomValues(new Uint8Array(16))
 
-      const crypto_key = await window.crypto.subtle.importKey(
-        'raw',
-        encryption_key,
-        { name: 'AES-CBC' },
-        false,
-        ['encrypt'],
-      )
+        const crypto_key = await window.crypto.subtle.importKey(
+          'raw',
+          encryption_key,
+          { name: 'AES-CBC' },
+          false,
+          ['encrypt'],
+        )
 
-      const encrypted_data = await window.crypto.subtle.encrypt(
-        { name: 'AES-CBC', iv: iv },
-        crypto_key,
-        encoded_data,
-      )
+        const encrypted_data = await window.crypto.subtle.encrypt(
+          { name: 'AES-CBC', iv: iv },
+          crypto_key,
+          encoded_data,
+        )
 
-      // Prepend IV to ciphertext.
-      const ciphertext_with_iv = new Uint8Array(
-        iv.byteLength + encrypted_data.byteLength,
-      )
-      ciphertext_with_iv.set(new Uint8Array(iv), 0)
-      ciphertext_with_iv.set(new Uint8Array(encrypted_data), iv.byteLength)
+        const ciphertext_with_iv = new Uint8Array(
+          iv.byteLength + encrypted_data.byteLength,
+        )
+        ciphertext_with_iv.set(new Uint8Array(iv), 0)
+        ciphertext_with_iv.set(new Uint8Array(encrypted_data), iv.byteLength)
 
-      return btoa(
-        ciphertext_with_iv.reduce(
-          (data, byte) => data + String.fromCharCode(byte),
-          '',
-        ),
-      )
+        return btoa(
+          ciphertext_with_iv.reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            '',
+          ),
+        )
+      } catch (error) {
+        console.error('Encryption failed: ', error)
+        throw new Error('Encryption failed')
+      }
     }
 
     export const decrypt = async (
       encrypted_data_with_iv: string,
       encryption_key: Uint8Array,
     ): Promise<string> => {
-      const encrypted_data_array_buffer = Uint8Array.from(
-        atob(encrypted_data_with_iv),
-        (c) => c.charCodeAt(0),
-      )
-      const iv = new Uint8Array(encrypted_data_array_buffer.slice(0, 16)) // Extract IV from ciphertext.
-      const encrypted_data = encrypted_data_array_buffer.slice(16)
+      try {
+        const encrypted_data_array_buffer = Uint8Array.from(
+          atob(encrypted_data_with_iv),
+          (c) => c.charCodeAt(0),
+        )
+        const iv = new Uint8Array(encrypted_data_array_buffer.slice(0, 16))
+        const encrypted_data = encrypted_data_array_buffer.slice(16)
 
-      const crypto_key = await window.crypto.subtle.importKey(
-        'raw',
-        encryption_key,
-        { name: 'AES-CBC' },
-        false,
-        ['decrypt'],
-      )
+        const crypto_key = await window.crypto.subtle.importKey(
+          'raw',
+          encryption_key,
+          { name: 'AES-CBC' },
+          false,
+          ['decrypt'],
+        )
 
-      const decrypted_data = await window.crypto.subtle.decrypt(
-        { name: 'AES-CBC', iv: iv },
-        crypto_key,
-        encrypted_data,
-      )
+        const decrypted_data = await window.crypto.subtle.decrypt(
+          { name: 'AES-CBC', iv: iv },
+          crypto_key,
+          encrypted_data,
+        )
 
-      return new TextDecoder().decode(decrypted_data)
+        return new TextDecoder().decode(decrypted_data)
+      } catch (error) {
+        console.error('Decryption failed: ', error)
+        throw new Error('Decryption failed')
+      }
     }
   }
 }
