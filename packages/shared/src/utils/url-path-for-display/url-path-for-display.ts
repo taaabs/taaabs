91c698f4027f5dcb91081ffe30683e2517b237b7
Robert Piosik <robertpiosik@gmail.com>
2024-06-26 00:34:52 +0200

    Refactored url-path-for-display function.

--- packages/shared/src/utils/url-path-for-display/url-path-for-display.ts
+++ packages/shared/src/utils/url-path-for-display/url-path-for-display.ts
@@ -4,29 +4,27 @@ export const url_path_for_display = (params: {
 }): string => {
   let parsed_url = params.url.replace('://', '')
 
-  if (parsed_url.substring(parsed_url.length - 1, parsed_url.length) == '/') {
+  // Remove trailing slash if present.
+  if (parsed_url.endsWith('/')) {
     parsed_url = parsed_url.substring(0, parsed_url.length - 1)
   }
 
-  if (
-    parsed_url.substring(parsed_url.length - 11, parsed_url.length) ==
-    '/index.html'
-  ) {
-    parsed_url = parsed_url.substring(0, parsed_url.length - 11)
-  } else if (
-    parsed_url.substring(parsed_url.length - 10, parsed_url.length) ==
-    '/index.htm'
-  ) {
-    parsed_url = parsed_url.substring(0, parsed_url.length - 10)
-  } else if (
-    parsed_url.substring(parsed_url.length - 5, parsed_url.length) == '.html'
-  ) {
-    parsed_url = parsed_url.substring(0, parsed_url.length - 5)
-  } else if (
-    parsed_url.substring(parsed_url.length - 4, parsed_url.length) == '.htm'
-  ) {
-    parsed_url = parsed_url.substring(0, parsed_url.length - 4)
+  // Strip common URL endings.
+  const remove_ending_from_url = (url: string) => {
+    switch (true) {
+      case url.endsWith('/index.html'):
+        return url.substring(0, url.length - 11)
+      case url.endsWith('/index.htm'):
+        return url.substring(0, url.length - 10)
+      case url.endsWith('.html'):
+        return url.substring(0, url.length - 5)
+      case url.endsWith('.htm'):
+        return url.substring(0, url.length - 4)
+    }
+    return url
   }
+
+  parsed_url = remove_ending_from_url(parsed_url)
 
   const parsed_url_arr = parsed_url.split('/')
   parsed_url_arr.shift()
