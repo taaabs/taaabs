507063549b30438d773f121a8669fb431b9cefff
Robert Piosik <robertpiosik@gmail.com>
2024-06-27 14:56:25 +0200

    Added hover effect for stars and conditional will-unselect classSupport: Added hook for will-unselect state change based on no selected index (#66)

--- packages/web-ui/src/components/common/Dropdown/stars/stars.module.scss.d.ts
+++ packages/web-ui/src/components/common/Dropdown/stars/stars.module.scss.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly container: string
+  readonly 'container--will-unselect': string
   readonly star: string
   readonly 'star--checked': string
   readonly 'star--hovered': string
--- packages/web-ui/src/components/common/Dropdown/stars/stars.tsx
+++ packages/web-ui/src/components/common/Dropdown/stars/stars.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Icon } from '../../particles/icon'
 import styles from './stars.module.scss'
 import cn from 'classnames'
+import useUpdateEffect from 'beautiful-react-hooks/useUpdateEffect'
 
 export namespace Stars {
   export type Props = {
@@ -12,9 +13,22 @@ export namespace Stars {
 
 export const Stars: React.FC<Stars.Props> = (props) => {
   const [hovered_index, set_hovered_index] = useState<number>()
+  const [will_unselect, set_will_unselect] = useState<boolean>()
+
+  useUpdateEffect(() => {
+    if (hovered_index == props.no_selected - 1) {
+      set_will_unselect(true)
+    } else {
+      set_will_unselect(false)
+    }
+  }, [hovered_index])
 
   return (
-    <button className={styles.container}>
+    <button
+      className={cn(styles.container, {
+        [styles['container--will-unselect']]: will_unselect,
+      })}
+    >
       {[...new Array(5)].map((_, i) => (
         <div
           className={cn([
