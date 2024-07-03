ad707c0d8abff21b81d528852907cc9c0c02ac15
Robert Piosik <robertpiosik@gmail.com>
2024-07-03 18:54:49 +0200

    Fix bookmark delete modal (#87)

--- packages/web-ui/src/components/common/templates/modal/content-standard/delete/delete.module.scss.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly container: string
-  readonly 'bookmark-title': string
-}
-export = styles
--- packages/web-ui/src/components/common/templates/modal/content-standard/delete/delete.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Delete } from './delete'
-
-export default {
-  component: Delete,
-}
-
-export const Primary = () => {
-  return <Delete label="Lorem ipsum">content</Delete>
-}
