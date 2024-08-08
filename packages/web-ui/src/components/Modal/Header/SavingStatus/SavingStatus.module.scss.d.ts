e3464cc79430aea225cf5cde164df2ceb5c1ff9b
Robert Piosik <robertpiosik@gmail.com>
2024-08-08 02:29:41 +0200

    When unsorted bookmark on filter [unsorted] gets tag, it should be removed from list (#185)

--- packages/web-ui/src/components/Modal/Header/SavingStatus/SavingStatus.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "container": string;
-  readonly "spin": string;
-  readonly "spinner": string;
-  readonly "text": string;
-  readonly "tick": string;
-};
-export = styles;
-
