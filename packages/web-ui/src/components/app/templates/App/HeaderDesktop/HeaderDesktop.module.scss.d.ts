b80e8c520a0df5fe344eff460eae03dd927adc92
Robert Piosik <robertpiosik@gmail.com>
2025-03-31 01:26:59 +0200

    Remove unused SCSS foundation imports across multiple components for cleaner code

--- packages/web-ui/src/components/app/templates/App/HeaderDesktop/HeaderDesktop.module.scss.d.ts
+++ packages/web-ui/src/components/app/templates/App/HeaderDesktop/HeaderDesktop.module.scss.d.ts
@@ -1,6 +1,7 @@
 declare const styles: {
-  readonly container: string
-  readonly inner: string
-  readonly navigation: string
-}
-export = styles
+  readonly "container": string;
+  readonly "inner": string;
+  readonly "navigation": string;
+};
+export = styles;
+
